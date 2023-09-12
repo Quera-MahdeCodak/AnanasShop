f4a55d3b718f38e210cf1c2ad1723a8ae9e96c1d
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2023-09-12 13:09:14 +0330

    implement forms.py, test_forms_sample.py

--- pineapple/forms.py
+++ pineapple/forms.py
@@ -1,16 +1,85 @@
+from django import forms
+from .models import *
+from django.core.validators import MinLengthValidator,RegexValidator,EmailValidator
+from django.core.exceptions import ValidationError
 
 
-class SellerForm:
-    pass
+def validate_price_toman(value):
+    if value < 1000:
+        raise ValidationError('قیمت نباید کمتر از هزار تومان باشد.')
+    if value > 1_000_000:
+        raise ValidationError('قیمت نباید از یک میلیون تومان بیشتر باشد.')
+    return value
 
-class PineappleForm:
-    pass
 
-class OrderForm:
-    pass
+def validate_weight_kg(value):
+    if value > 100:
+        raise ValidationError('۱۰۰ کیلو آناناس میخوای چیکار؟ مشکل داری؟')
+    return value
 
-class SubscriptionForm:
-    pass
 
-class CommentForm:
-    pass
+class SellerForm(forms.ModelForm):
+    class Meta:
+        model = Seller
+        fields = ["name", "address", "certificate_code"]
+
+    name = forms.CharField(max_length=256)
+    address = forms.CharField(
+        validators=[
+            MinLengthValidator(limit_value=10, message="این فیلد باید بیشتر از ۱۰ کاراکتر باشد.")
+        ]
+    )
+    certificate_code = forms.CharField(
+        max_length=10,
+        validators=[
+            RegexValidator('^[A-Z]+$', message="حروف گواهینامه باید حروف بزرگ باشد.")
+        ]
+    )
+
+
+class PineappleForm(forms.ModelForm):
+    class Meta:
+        model = Pineapple
+        fields = ["price_toman", "seller"]
+
+    seller = forms.ModelChoiceField(queryset=Seller.objects.all(),empty_label="فروشنده")
+    price_toman = forms.IntegerField(
+        label="قیمت",
+        validators=[validate_price_toman]
+    )
+
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = ["pineapple", "name", "weight_kg"]
+
+    pineapple = forms.ModelChoiceField(queryset=Pineapple.objects.all(),empty_label="آناناس")
+    name = forms.CharField()
+    weight_kg = forms.FloatField(validators=[validate_weight_kg])
+
+
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscription
+        fields = ["name","phone_number"]
+
+    name = forms.CharField()
+    phone_number = forms.CharField(
+        validators=[
+            RegexValidator('^09[0-9]{9}$',message='شماره تلفن اشتباه است. شماره تلفن باید ۱۱ رقم باشد و با ۰۹ شروع شود.')
+        ]
+    )
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["seller", "name", "text"]
+
+    seller = forms.ModelChoiceField(queryset=Seller.objects.all(), empty_label="فروشنده")
+    name = forms.CharField(max_length=50)
+    text = forms.CharField(
+        validators=[MinLengthValidator(limit_value=10, message='این فیلد باید بیشتر از ۱۰ کاراکتر باشد.')]
+    )
--- tests/test_forms_sample.py
+++ tests/test_forms_sample.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from pineapple.forms import CommentForm
+from pineapple.forms import *
 from pineapple.models import Seller, Pineapple
 
 
@@ -20,13 +20,19 @@ class FormValidationTest(TestCase):
         self.seller1 = {
             'name': "TestSeller",
             'address': "Test",
-            'certificate_code': "CSWECx"
+            'certificate_code': "CSWECX"
         }
 
         self.seller2 = {
             'name': "TestSeller",
             'address': "Quera QueraNejad Road",
             'certificate_code': "CSWECX"
+        }
+
+        self.seller3 = {
+            'name': "TestSeller",
+            'address': "Quera QueraNejad Road",
+            'certificate_code': "CSWECx"
         }
         
         self.pineapple1 = {
@@ -42,24 +48,66 @@ class FormValidationTest(TestCase):
             'seller': self.seller
         }
         
-        self.order = {
+        self.order1 = {
+            'pineapple': self.pineapple,
+            'name': "Test Order",
+            'weight_kg': 50
+        }
+        self.order2 = {
             'pineapple': self.pineapple,
             'name': "Test Order",
             'weight_kg': 150
         }
-        
-        
+
         self.comment = {
             'seller': self.seller,
             'name': "Test Comment",
             'text': "This"
         }
         
-        self.subscription = {
+        self.subscription1 = {
             'name': "Test Subscriber",
             'phone_number': "12345678901"
+        }
+
+        self.subscription2 = {
+            'name': "Test Subscriber",
+            'phone_number': "09114412191"
         }
 
     def test_comment_form(self):
         form = CommentForm(data=self.comment)
         self.assertFalse(form.is_valid())
+
+    def test_seller_form(self):
+        form1 = SellerForm(data=self.seller1)
+        self.assertFalse(form1.is_valid())
+
+        form3 = SellerForm(data=self.seller3)
+        self.assertFalse(form3.is_valid())
+
+        form2 = SellerForm(data=self.seller2)
+        self.assertTrue(form2.is_valid())
+
+    def test_pineapple_form(self):
+        form1 = PineappleForm(data=self.pineapple1)
+        self.assertFalse(form1.is_valid())
+
+        form2 = PineappleForm(data=self.pineapple2)
+        self.assertFalse(form2.is_valid())
+
+        form3 = PineappleForm(data=self.pineapple3)
+        self.assertTrue(form3.is_valid())
+
+    def test_order_form(self):
+        form1 = OrderForm(data=self.order1)
+        self.assertTrue(form1.is_valid())
+        form2 = OrderForm(data=self.order2)
+        self.assertFalse(form2.is_valid())
+
+    def test_subscription_form(self):
+        form1 = SubscriptionForm(data=self.subscription1)
+        self.assertFalse(form1.is_valid())
+        form2 = SubscriptionForm(data=self.subscription2)
+        self.assertTrue(form2.is_valid())
+
