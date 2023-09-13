333e8920932f6decf185c945221f8c79863584cf
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2023-09-13 14:24:39 +0330

    update urls

--- pineapple/urls.py
+++ pineapple/urls.py
@@ -5,27 +5,27 @@ from django.conf.urls.static import static
 app_name = "pineapple"
 
 subscription_patterns = [
-    path('subscription-create', views.subscription_create_view, name='subscription-create'),
-    path('subscription-list', views.subscription_list_view, name='subscription-list'),
+    path('subscription/create', views.subscription_create_view, name='subscription-create'),
+    path('subscriptions', views.subscription_list_view, name='subscription-list'),
 ]
 seller_patterns = [
-    path('seller-list', views.seller_list_view,name='seller-list'),
-    path('seller-detail/<str:certificateCode>', views.seller_detail_view,name='seller-detail'),
-    path('seller-create', views.seller_create_view,name='seller-create'),
-    path('seller-update', views.seller_update_view,name='seller-update'),
+    path('sellers', views.seller_list_view,name='seller-list'),
+    path('seller/<str:certificateCode>', views.seller_detail_view,name='seller-detail'),
+    path('seller/create', views.seller_create_view,name='seller-create'),
+    path('seller/update/<str:certificateCode>', views.seller_update_view,name='seller-update'),
 ]
 pineapple_patterns = [
-    path('pineapple-list', views.pineapple_list_view, name='pineapple-list'),
-    path('pineapple-detail/<int:pineappleId>', views.pineapple_detail_view, name='pineapple-detail'),
-    path('pineapple-create', views.pineapple_create_view, name='pineapple-create'),
-    path('pineapple-update', views.pineapple_update_view, name='pineapple-update'),
-    path('seller-pineapple-list/<str:certificate_code>', views.seller_pineapple_list_view, name='pineapple-update'),
+    path('pineapples', views.pineapple_list_view, name='pineapple-list'),
+    path('pineapple/<int:pineappleId>', views.pineapple_detail_view, name='pineapple-detail'),
+    path('pineapple/create', views.pineapple_create_view, name='pineapple-create'),
+    path('pineapple/update/<int:pineappleId>', views.pineapple_update_view, name='pineapple-update'),
+    path('seller/<str:certificate_code>/pineapples', views.seller_pineapple_list_view, name='pineapple-update'),
 ]
 order_pattern =[
-    path('order-list', views.order_list_view,name='order-list'),
-    path('order-detail/<int:orderId>', views.order_detail_view,name='order-detail'),
-    path('order-create', views.order_create_view,name='order-create'),
-    path('order-update', views.order_update_view,name='order-update'),
+    path('orders', views.order_list_view,name='order-list'),
+    path('order/<int:orderId>', views.order_detail_view,name='order-detail'),
+    path('order/create', views.order_create_view,name='order-create'),
+    path('order/update/<int:orderId>', views.order_update_view,name='order-update'),
 ]
 
 urlpatterns = [
