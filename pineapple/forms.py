2b45ac111265a4df5ed57691e317b752f4a980a6
Monireh Bastami <monirehbastami@gmail.com>
2023-09-12 19:35:02 -0700

    Feat: Add SellerForm in pineapple-forms

--- pineapple/forms.py
+++ pineapple/forms.py
@@ -1,7 +1,15 @@
+from django import forms
+from .models import *
 
-
-class SellerForm:
-    pass
+class SellerForm(forms.Form):
+    name = forms.CharField(required=True,label="نام فروشنده",max_length=256)
+    address = forms.CharField(required=True,label="آدرس",widget=forms.TextInput)
+    certificate_code = forms.CharField(required=True,max_length=10)
+    def clean_address(self):
+        address = self.cleaned_data.get('address')
+        if len(address) < 10:
+            raise forms.ValidationError("این فیلد باید بیشتر از ۱۰ کاراکتر باشد.")
+        return address
 
 class PineappleForm:
     pass
