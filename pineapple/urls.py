801e6f85b5c5a1b42c724d6d8d3e929ae70e43f0
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2023-09-14 14:44:32 +0330

    add seller urls

--- pineapple/urls.py
+++ pineapple/urls.py
@@ -8,6 +8,11 @@ app_name = "pineapple"
 urlpatterns = [
     path("subscription/create", views.subscription_create_view ,name="subscription-create"),
     path("subscriptions", views.subscription_list_view, name="subscription-list"),
+
+    path('sellers', views.seller_list_view, name='seller-list'),
+    path('seller/create', views.seller_create_view, name='seller-create'),
+    path('seller/detail/<str:certificate_code>', views.seller_detail_view, name='seller-detail'),
+    path('seller/update/<str:certificate_code>', views.seller_update_view, name='seller-update'),
 ]
 
 if settings.DEBUG:
