1a55284c7bf42e75dcf7c6e7733be1050935f1b1
Monireh Bastami <monirehbastami@gmail.com>
2023-09-13 23:07:38 -0700

    update view_sellers file

--- pineapple/views/views_sellers.py
+++ pineapple/views/views_sellers.py
@@ -6,34 +6,38 @@ from pineapple.forms import SellerForm
 
 def seller_list_view(request):
     sellers = Seller.objects.all()
-    return render(request, 'sellers/seller_list.html', {'sellers': sellers})
+    context = {'sellers': sellers}
+    return render(request, 'sellers/seller_list.html', context)
 
 def seller_detail_view(request, certificate_code):
     seller = get_object_or_404(Seller, certificate_code=certificate_code)
     return render(request, 'sellers/seller_detail.html', {'seller': seller})
 
 def seller_create_view(request):
+    form = SellerForm()
     if request.method == "POST":
         form = SellerForm(request.POST)
         if form.is_valid():
-            form.save()
+            obj = Seller.objects.create(name=form.cleaned_data['name'],address=form.cleaned_data['address'],certificate_code=form.cleaned_data['certificate_code'])
+            obj.save()
             return redirect('pineapple:seller-list') 
     else:
         try:
             form = SellerForm()
         except Exception:
             form = "فرم پیاده سازی نشده است."
-
-    return render(request, 'sellers/seller_create.html', {'form': form})
+    context = {'form': form}
+    return render(request, 'sellers/seller_create.html', context)
 
 
 def seller_update_view(request, certificate_code):
     seller = get_object_or_404(Seller, certificate_code=certificate_code)
-
+    obj = Seller.objects.filter(certificate_code=certificate_code)
     if request.method == "POST":
         form = SellerForm(request.POST, instance=seller)
         if form.is_valid():
-            form.save()
+            data = form.cleaned_data
+            obj.update(**data)
             return redirect('pineapple:seller-list')
     else:
         try:
