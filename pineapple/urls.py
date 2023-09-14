4d7c8aa27ee497c63cf10bf9bf921e6a8dfda448
Arian <mha.september.2003@gmail.com>
2023-09-14 14:35:02 +0330

    add subscription urls

--- pineapple/urls.py
+++ pineapple/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 app_name = "pineapple"
 
 urlpatterns = [
-    
+    path("subscription/create", views.subscription_create_view ,name="subscription-create"),
+    path("subscriptions", views.subscription_list_view, name="subscription-list"),
 ]
 
 if settings.DEBUG:
