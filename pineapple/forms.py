e85588611f86ad2b664a4d214c54f24675274ad4
Sam1AT <alileon32@gmail.com>
2023-09-14 15:18:01 +0330

    adding SellerForm to forms

--- pineapple/forms.py
+++ pineapple/forms.py
@@ -1,7 +1,23 @@
+from django import forms
+from .models import *
+import re
 
-
-class SellerForm:
-    pass
+class SellerForm(forms.ModelForm):
+    class Meta:
+        model = Seller
+        fields = "__all__"
+    
+    def clean_certificate_code(self):
+        certificate_code = self.cleaned_data['certificate_code']
+        if not certificate_code.isupper():
+            raise forms.ValidationError('حروف گواهینامه باید حروف بزرگ باشد.')
+        return certificate_code
+    
+    def clean_address(self):
+        address = self.cleaned_data['address']
+        if len(address) < 10:
+            raise forms.ValidationError('این فیلد باید بیشتر از ۱۰ کاراکتر باشد.')
+        return address
 
 class PineappleForm:
     pass
