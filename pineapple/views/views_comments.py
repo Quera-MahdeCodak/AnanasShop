0868999f796e327712899eb2bb4a4f419e39eb1f
Monireh Bastami <monirehbastami@gmail.com>
2023-09-13 23:06:09 -0700

    update vie_comments file

--- pineapple/views/views_comments.py
+++ pineapple/views/views_comments.py
@@ -3,16 +3,23 @@ from django.shortcuts import render, redirect, get_object_or_404
 from pineapple.models import Comment, Seller
 from pineapple.forms import CommentForm
 
+def comment_list_view(request):
+    comments = Comment.objects.all()
+    return render(request, 'comments/comments_list.html', {'comments': comments})
+
 def seller_comment_list_view(request, certificate_code):
     seller = Seller.objects.get(certificate_code=certificate_code)
     comments = Comment.objects.filter(seller=seller)
     return render(request, 'comments/seller_comment_list.html', {'comments': comments, 'seller': seller})
 
 def comment_create_view(request):
+    form = CommentForm()
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
-            form.save()
+            selectedSeller=Seller.objects.get(id=form.cleaned_data['seller'])
+            obj = Comment.objects.create(seller=selectedSeller,name=form.cleaned_data['name'],text=form.cleaned_data['text'])
+            obj.save()
             return redirect('pineapple:comment-list')
     else:
         try:
